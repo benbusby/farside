f33279c41b17545bfbe7c0f9baa5d1b49c814a30
Ben Busby <noreply+git@benbusby.com>
2021-11-10 11:47:03 -0700

    Test for valid service redirects
    Updates testing to iterate through all available services and validate
    that each one correctly produces a 302 redirect.

--- test/farside_test.exs
+++ test/farside_test.exs
@@ -1,4 +1,6 @@
 defmodule FarsideTest do
+  @services_json Application.fetch_env!(:farside, :services_json)
+
   use ExUnit.Case
   use Plug.Test
 
@@ -14,5 +16,41 @@ defmodule FarsideTest do
 
     assert conn.state == :sent
     assert conn.status == 200
+  end
+
+  test "/ping" do
+    conn =
+      :get
+      |> conn("/ping", "")
+      |> Router.call(@opts)
+
+    assert conn.state == :sent
+    assert conn.status == 200
+    assert conn.resp_body == "PONG"
+  end
+
+  test "/:service" do
+    {:ok, file} = File.read(@services_json)
+    {:ok, service_list} = Poison.decode(file, as: [%{}])
+
+    service_names =
+      Enum.map(
+        service_list,
+        fn service -> service["type"] end
+      )
+
+    IO.puts("")
+
+    Enum.map(service_names, fn service_name ->
+      IO.puts("/#{service_name}")
+
+      conn =
+        :get
+        |> conn("/#{service_name}", "")
+        |> Router.call(@opts)
+
+      assert conn.state == :set
+      assert conn.status == 302
+    end)
   end
 end
